912f8d7840745ce3fed7be5ba2384cc7b44b9b6a
Raden616Rizki <rizkiraden616@gmail.com>
2023-12-03 19:26:20 +0700

    Commit Update input and add post method on app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from static.python import maut
 
 app = Flask(__name__)
 
@@ -8,7 +9,17 @@ def index():
 
 @app.route('/post_maut', methods=['POST'])
 def post_maut():
-    pass
+    matriks = request.form.get('matriks')
+    jensi = request.form.get('jensi')
+    bobot = request.form.get('bobot')
+    
+    matriks = json.loads(matriks)
+    jenis = json.loads(jenis)
+    bobot = json.loads(bobot)
+    
+    result = maut.initiation(matriks, jenis, bobot)
+    
+    return jsonify({'message': 'success', 'result': result})
 
 if (__name__ == '__main__'):
     app.run('0.0.0.0', port=5000, debug=True)
