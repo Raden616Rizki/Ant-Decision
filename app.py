3462a8cb150b18b54151f915c762978fe55c6ae1
Raden616Rizki <rizkiraden616@gmail.com>
2023-12-03 19:13:32 +0700

    Commit Icon Tab

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/post_maut', methods=['POST'])
+def post_maut():
+    pass
+
 if (__name__ == '__main__'):
     app.run('0.0.0.0', port=5000, debug=True)
